3bf2222210ed334ade4980a7ff4d253e748c527f
joshactive <josh@activeaway.com>
2025-11-13 17:28:44 +0000

    events single page

---
+++ src/api/events-page/controllers/events-page.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * events-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::events-page.events-page');
+
---
+++ src/api/events-page/services/events-page.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * events-page service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::events-page.events-page');
+
