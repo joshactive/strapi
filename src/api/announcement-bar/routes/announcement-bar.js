92902f570d62ae59b571f9b8fb392489053a7fdc
joshactive <josh@activeaway.com>
2025-11-15 13:39:45 +0000

    announcement bar

---
+++ src/api/announcement-bar/routes/announcement-bar.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * announcement-bar router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::announcement-bar.announcement-bar');
+
---
+++ src/api/announcement-bar/services/announcement-bar.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * announcement-bar service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::announcement-bar.announcement-bar');
+
