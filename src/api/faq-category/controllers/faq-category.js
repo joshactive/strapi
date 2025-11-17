893b0eb5b9aaca65696e48c84e97899d67e1b27a
joshactive <josh@activeaway.com>
2025-11-17 11:34:12 +0000

    faqs

---
+++ src/api/faq-category/controllers/faq-category.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * faq-category controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::faq-category.faq-category');
+
---
+++ src/api/faq-category/services/faq-category.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * faq-category service
+ */
+
+const { createCoreService } = require('@strapi/strapi').factories;
+
+module.exports = createCoreService('api::faq-category.faq-category');
+
