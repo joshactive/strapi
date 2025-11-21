41d5a858921a3bdd67d7d3c0a3d100dcc73d790f
joshactive <josh@activeaway.com>
2025-11-21 21:03:18 +0000

    privacy policy

---
+++ src/api/privacy-policy-page/controllers/privacy-policy-page.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * privacy-policy-page controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::privacy-policy-page.privacy-policy-page');
+
